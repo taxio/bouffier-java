942b67a3a51a88e126a66b15be58b4d0f09378b5
taxio <taxio.2029@gmail.com>
2019-08-07 10:30:47 +0900

    v0.2.0

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -35,7 +35,7 @@ public class Main {
     static FormatType format;
     static Path projectPath;
     static ParseMode mode;
-    static final String version = "v0.1.0";
+    static final String version = "v0.2.0";
 
     public static void main(String[] args) {
         System.out.println("---------------------------------------------------\n");
