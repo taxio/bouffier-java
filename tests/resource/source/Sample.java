ce8382ab05a2cd625cd22f0a03c1278ab973ef3a
taxio <taxio.2029@gmail.com>
2020-01-07 16:57:53 +0900

    fix

---
+++ tests/resource/source/Sample.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Hoge {
+    /**
+     * return constant
+     *
+     * @return const number
+     */
+    static int hoge() {
+        // return constant
+        return 1;
+    }
+}
